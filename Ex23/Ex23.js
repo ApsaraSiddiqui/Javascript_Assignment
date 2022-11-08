9c69b36160714cb15380a6c94a96d71344d61aef
Apsara Siddiqui <114807068+ApsaraSiddiqui@users.noreply.github.com>
2022-11-09 02:14:09 +0500

    Add files via upload

---
+++ Ex23/Ex23.js
@@ -0,0 +1,113 @@
+/*Conditional Tests: Write a series of conditional tests. Print a statement describing each test and your prediction for the results of each test.
+ Your code should look something like this:
+
+let car = 'subaru';
+
+console.log("Is car == 'subaru'? I predict True.")
+
+console.log(car == 'subaru')
+
+• Look closely at your results, and make sure you understand why each line evaluates to True or False.
+
+• Create at least 10 tests. Have at least 5 tests evaluate to True and another 5 tests evaluate to False.*/
+
+
+let course=["Computer","Science","Urdu","English","Geography","Isl","Litertature","History","Chemistry","Physics"]
+
+if(course[0]=="Computer")
+{
+    console.log("true")
+}
+else
+{
+    console.log("false")
+}
+
+
+if(course[1]=="Science")
+{
+    console.log("true")
+}
+else
+{
+    console.log("false")
+}
+
+
+if(course[2]=="Urdu")
+{
+    console.log("true")
+}
+else
+{
+    console.log("false")
+}
+
+
+if(course[3]=="English")
+{
+    console.log("true")
+}
+else
+{
+    console.log("false")
+}
+
+
+if(course[4]=="Geography")
+{
+    console.log("true")
+}
+else
+{
+    console.log("false")
+}
+
+if(course[5]=="Computer")
+{
+    console.log("true")
+}
+else
+{
+    console.log("false")
+}
+
+
+if(course[6]=="Science")
+{
+    console.log("true")
+}
+else
+{
+    console.log("false")
+}
+
+
+if(course[7]=="Urdu")
+{
+    console.log("true")
+}
+else
+{
+    console.log("false")
+}
+
+
+if(course[8]=="English")
+{
+    console.log("true")
+}
+else
+{
+    console.log("false")
+}
+
+
+if(course[9]=="Geography")
+{
+    console.log("true")
+}
+else
+{
+    console.log("false")
+}
