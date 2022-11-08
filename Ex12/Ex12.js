7093545938649f6b1171b1af18a4245a6c5d847b
Apsara Siddiqui <114807068+ApsaraSiddiqui@users.noreply.github.com>
2022-11-09 01:49:20 +0500

    Add files via upload

---
+++ Ex12/Ex12.js
@@ -0,0 +1,11 @@
+/*Greetings: Start with the array you used in Exercise 11, but instead of just printing each person’s name, print a message to them. 
+The text of each message should be the same, but each message should be personalized with the person’s name.*/
+
+
+
+var names=["Kiran","Apsara","Nimra"]
+
+for(var i=0;i<names.length;i++)
+{
+    console.log("Hello,",names[i])
+}
