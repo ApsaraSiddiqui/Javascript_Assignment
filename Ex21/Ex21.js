872ab8bd8e697f4d21fa3a7f8581b5d4737d0acb
Apsara Siddiqui <114807068+ApsaraSiddiqui@users.noreply.github.com>
2022-11-09 02:09:50 +0500

    Add files via upload

---
+++ Ex21/Ex21.js
@@ -0,0 +1,9 @@
+/*They think of something you could store in a JavaScript Object. Write a program that creates Objects containing these items.*/
+
+let list={
+    uk_city: "London",
+    france_city: "Paris",
+    australia_city: "Sydney"
+
+}
+
