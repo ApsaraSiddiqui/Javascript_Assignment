8283da98a0e70aeb89b922d11292c359a7a1b4c6
Apsara Siddiqui <114807068+ApsaraSiddiqui@users.noreply.github.com>
2022-11-09 01:54:52 +0500

    Add files via upload

---
+++ Ex14/Ex14.js
@@ -0,0 +1,12 @@
+/*Guest List: If you could invite anyone, living or deceased, to dinner, who would you invite? 
+Make a list that includes at least three people you’d like to invite to dinner. 
+Then use your list to print a message to each person, inviting them to dinner.*/
+
+
+
+var Guest=["Ali","Aisha","Areeba"]
+
+for(var i=0;i<Guest.length;i++)
+{
+    console.log("I would like to invite",Guest[i] ,"to dinner")
+}
