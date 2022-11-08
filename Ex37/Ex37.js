8b8b22ebf0383e8d9899ad9ee17089e1eddb1850
Apsara Siddiqui <114807068+ApsaraSiddiqui@users.noreply.github.com>
2022-11-09 02:39:29 +0500

    Add files via upload

---
+++ Ex37/Ex37.js
@@ -0,0 +1,14 @@
+/*Large Shirts: Modify the make_shirt() function so that shirts are large by default with a message that reads I love JavaScript.\
+ Make a large shirt and a medium shirt with the default message, and a shirt of any size with a different message.*/
+
+
+ 
+ function make_shirt(size="large",text="I love JavaScript")
+ {
+    console.log("The size of shirt is",size," and text should be written on it is",text)
+ }
+
+ make_shirt()
+ make_shirt("small","Never giveup")
+ make_shirt("medium","You can and you will")
+ 
