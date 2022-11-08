80df39e1496f7f909d72f98e733741e9bf62c31e
Apsara Siddiqui <114807068+ApsaraSiddiqui@users.noreply.github.com>
2022-11-09 01:38:33 +0500

    Add files via upload

---
+++ Ex8/Ex8.js
@@ -0,0 +1,7 @@
+//You should create four lines that look like this:
+
+
+ console.log("Addition:", 5+3)
+ console.log("Subtraction:", 9-1)
+ console.log("Multiplication:", 2*4)
+ console.log("Division:", 24/3)
