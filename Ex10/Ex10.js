23857c1aa666afd4320606bcfd8add616b38f059
Apsara Siddiqui <114807068+ApsaraSiddiqui@users.noreply.github.com>
2022-11-09 01:44:36 +0500

    Add files via upload

---
+++ Ex10/Ex10.js
@@ -0,0 +1,21 @@
+/*Adding Comments: Choose two of the programs you’ve written, and add at least one comment to each. 
+If you don’t have anything specific to write because your programs are too simple at this point, just add your name and 
+the current date at the top of each program file. 
+Then write one sentence describing what the program does.*/
+
+
+
+
+//Apsara Siddiqui
+//28-oct-2022
+
+console.log(`Grorge C.Lichtenberg once said," It is strange that only extraordinary men make the discoveries, which later appear so easy and simple."`)
+//printing a quotation 
+
+
+//Apsara Siddiqui
+//28-oct-2022
+console.log("Addition:", 5+3) // addition operation
+console.log("Subtraction:", 9-1) //subtraction operation
+console.log("Multiplication:", 2*4) //Multiplication operation
+console.log("Division:", 24/3) //Division operation
