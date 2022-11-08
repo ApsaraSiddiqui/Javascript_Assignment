e70d33eba1c5f980031ea412ea6686abfd0d70a3
Apsara Siddiqui <114807068+ApsaraSiddiqui@users.noreply.github.com>
2022-11-09 01:31:30 +0500

    Add files via upload

---
+++ Ex5/Ex5.js
@@ -0,0 +1,9 @@
+
+/*Famous Quote 2: Repeat Exercise 4, but this time store the famous person’s name in a variable called famous_person. 
+Then compose your message and store it in a new variable called message. Print your message.*/
+
+
+var famous_person="Grorge C.Lichtenberg"
+var message=`${famous_person} once said," It is strange that only extraordinary men make the discoveries, which later appear so easy and simple."`
+
+console.log(message)
